557e20aec7d59a549aa34b4c85babbc4ab66c15b
BrunoGattiSalinas <brunogattisalinas@hotmail.com>
2021-08-20 07:03:15 +0900

    Atualizado com buscar qrCode e o Botão de limpar

--- www/lib/script.js
+++ www/lib/script.js
@@ -112,5 +112,38 @@ fetch(`https://www.jussimarleal.com.br/exemplo_api/pessoa/${id.value }`, {
   id.value = "";
   LimparCampos();
 })
+  
+  function checkConnection() {
+    var networkState = navigator.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+
+    if(states[networkState] == 'No network connection'){
+       return false;
+    }else{
+      return true;
+    }
+}
+
+buscar, qrcode, cadastrar, alterar, deletar, limpar.addEventListener("offline", onOffline, false);
+
+  function confirma(buttonIndex){
+    if(buttonIndex == 1){
+      navigator.app.exitApp();
+    }
+  }
+  
+  function onOffline(){
+    navigator.notification.confirm("Sem Conexão á Internet, tente novamente mais tarde!",confirma,"Ops!",['Sair','Ok']);
+  }
+
   }
   
